87ce9d7073cd7dc552bbe3c36722f753866e8432
shadow3731 <v1rus3731@gmail.com>
2024-07-25 14:08:42 +0300

    fix 1.8.1

--- dist/Network Folders/_internal/Network Folders/main.py
+++ dist/Network Folders/_internal/Network Folders/main.py
@@ -1,27 +1,22 @@
 if __name__ == '__main__':
-    try:
-        import sys
+    import sys
 
-        if getattr(sys, 'frozen', False):
-            app_dir = f'{sys._MEIPASS}\\Network Folders'
+    if getattr(sys, 'frozen', False):
+        app_dir = f'{sys._MEIPASS}\\Network Folders'
         
-        else:
-            from pathlib import Path
-            app_dir = Path(__file__).resolve().parent
+    else:
+        from pathlib import Path
+        app_dir = Path(__file__).resolve().parent
 
-        from app.app import Application
-        app = Application(app_dir)
-        app.acquire_lock()
+    from app.app import Application
+    app = Application(app_dir)
+    app.acquire_lock()
 
-        update = app.try_to_update()
+    update = app.try_to_update()
             
-        if update is False:
-            app.start()
+    if update is False:
+        app.start()
                 
-        app.release_lock()
-    
-    except Exception as e:
-        with open('e_log.log', 'w') as f:
-            f.write(e)
+    app.release_lock()
         
     sys.exit(0)
